f4c8c229fcd40d92ba8e9cced33d24c8a798918d
huyneeee <nqh.one.of.a.kind@gmail.com>
2024-07-14 21:32:34 +0700

    first commit

---
+++ app/(manage)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from 'react'
+
+export default function LayoutManage({ children}: PropsWithChildren) {
+  return (
+    <div>LayoutManage
+      {children}
+    </div>
+  )
+}
---
+++ app/(storefront)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from 'react'
+
+export default function StoreFrontLayout({ children} : PropsWithChildren) {
+  return (
+    <div>StoreFrontLayout
+      {children}
+    </div>
+  )
+}
